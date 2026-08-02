f1e1ed3cadccda3e6ac23fc7b62d09cd408a1664
saran <sn4551669@gmail.com>
2026-08-02 12:48:17 +0530

    Create 3945. Digit Frequency Score.java

---
+++ 3945. Digit Frequency Score.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+      int res=0;
+      while(n>0){
+        int d=n%10;
+        res+=d;
+        n/=10;
+      }  
+      return res;
+    }
+}
