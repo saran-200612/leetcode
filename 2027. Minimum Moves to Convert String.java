7884d18923cdcad7c5fe0e5b98b3758a81e0c510
saran <sn4551669@gmail.com>
2026-08-02 10:14:33 +0530

    Implement minimumMoves method to count moves

---
+++ 2027. Minimum Moves to Convert String.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int count=0;
+       int i=0;
+       while(i<s.length()){
+        if(s.charAt(i)=='X'){
+            i=i+3;
+            count++;}
+            else{
+                i++;
+            }
+        
+       }
+       
+       return count;
+    }
+}
